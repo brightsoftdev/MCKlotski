190d663ff6e6128efff32caa53f46499d60a6780
gtts <mcgtts@hotmail.com>
2012-04-21 15:42:05 +0800

    new project

---
+++ MCKlotski/MCKlotski/MCAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  MCAppDelegate.h
+//  MCKlotski
+//
+//  Created by lim edwon on 12-4-21.
+//  Copyright (c) 2012年 TJUT-SCCE-SIPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
